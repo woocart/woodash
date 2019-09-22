354d16043dacbe07007dd7d573c0283e3d334f49
Akshit Sethi <akshitsethi@gmail.com>
2019-09-22 21:56:58 +0800

    Include the classes

---
+++ src/classes/class-config.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Niteo\WooCart\WooDash {
+
+	/**
+	 * Configuration options for the plugin.
+	 *
+	 * @package    Niteo\WooCart\WooDash
+	 * @since      1.0.0
+	 */
+	class Config {
+
+    const DB_OPTION    		= 'woodash_options';
+		const PREFIX    			= 'woodash_';
+		const DEFAULT_STATUS 	= 'woodash';
+
+  }
+
+}
