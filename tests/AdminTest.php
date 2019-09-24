eea7368537b575dd8307b0da6d67f1c0dde264dc
Akshit Sethi <akshitsethi@gmail.com>
2019-09-24 15:34:35 +0800

    Fixes to the plugin

--- tests/AdminTest.php
+++ tests/AdminTest.php
@@ -25,7 +25,7 @@ class AdminTest extends TestCase {
 	public function testConstructor() {
 		$admin = new Admin();
 
-		\WP_Mock::expectActionAdded( 'admin_init', [ $admin, 'initialize' ], 10 );
+		\WP_Mock::expectActionAdded( 'wp_loaded', [ $admin, 'check_permissions' ], 10 );
 
 		$admin->__construct();
 		\WP_Mock::assertHooksAdded();
@@ -34,20 +34,14 @@ class AdminTest extends TestCase {
 
 	/**
 	 * @covers \Niteo\WooCart\WooDash\Admin::__construct
-	 * @covers \Niteo\WooCart\WooDash\Admin::initialize
+	 * @covers \Niteo\WooCart\WooDash\Admin::check_permissions
+	 * @covers \Niteo\WooCart\WooDash\Admin::woo_dashboard
 	 */
-	public function testInitialize() {
+	public function testCheckPermissions() {
 		$admin = new Admin();
 
 		\WP_Mock::userFunction(
 			'is_admin',
-			[
-				'return' => true,
-			]
-		);
-
-		\WP_Mock::userFunction(
-			'esc_url',
 			[
 				'return' => true,
 			]
@@ -61,7 +55,21 @@ class AdminTest extends TestCase {
 		);
 
 		\WP_Mock::userFunction(
+			'current_user_can',
+			[
+				'return' => true,
+			]
+		);
+
+		\WP_Mock::userFunction(
 			'sanitize_text_field',
+			[
+				'return' => true,
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_url',
 			[
 				'return' => true,
 			]
@@ -74,35 +82,26 @@ class AdminTest extends TestCase {
 			]
 		);
 
-		\WP_Mock::expectActionAdded( 'plugins_loaded', [ $admin, 'check_permissions' ], 10 );
-
-		$admin->initialize();
+		$admin->check_permissions();
 		\WP_Mock::assertHooksAdded();
 	}
 
+
 	/**
 	 * @covers \Niteo\WooCart\WooDash\Admin::__construct
-	 * @covers \Niteo\WooCart\WooDash\Admin::check_permissions
 	 * @covers \Niteo\WooCart\WooDash\Admin::woo_dashboard
 	 */
-	public function testCheckPermissions() {
+	public function testWooDashboard() {
 		$admin = new Admin();
-
-		\WP_Mock::userFunction(
-			'current_user_can',
-			[
-				'return' => true,
-			]
-		);
 
 		\WP_Mock::expectActionAdded( 'admin_menu', [ $admin, 'change_admin_menu' ], PHP_INT_MAX );
 		\WP_Mock::expectActionAdded( 'wp_dashboard_setup', [ $admin, 'dashboard_widgets' ], PHP_INT_MAX );
-		\WP_Mock::expectActionAdded( 'init', [ $admin, 'switch_dashboards' ], 10 );
+		\WP_Mock::expectActionAdded( 'admin_init', [ $admin, 'switch_dashboards' ], 10 );
 
 		\WP_Mock::expectFilterAdded( 'custom_menu_order', [ $admin, 'rearrange_admin_menu' ], PHP_INT_MAX, 1 );
 		\WP_Mock::expectFilterAdded( 'menu_order', [ $admin, 'rearrange_admin_menu' ], PHP_INT_MAX, 1 );
 
-		$admin->check_permissions();
+		$admin->woo_dashboard();
 		\WP_Mock::assertHooksAdded();
 	}
 
